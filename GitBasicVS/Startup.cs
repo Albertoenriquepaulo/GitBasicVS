46d5e1c55a6f0d0ba19813889f3a7377723c2aca
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-03-30 12:43:48 +0200

    tercer commit

--- GitBasicVS/Startup.cs
+++ GitBasicVS/Startup.cs
@@ -25,7 +25,6 @@ namespace GitBasicVS
         {
             services.AddControllersWithViews();
             int a = 10;
-            double c = a * 10;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
